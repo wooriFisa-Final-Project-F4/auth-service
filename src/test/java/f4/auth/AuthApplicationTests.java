36d251e956fdd7ce53e6c2630a255e96c3c0b60b
endlessmomo <soohyuk96@naver.com>
2023-08-20 12:23:19 +0900

    [build] : environment settings

---
+++ src/test/java/f4/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package f4.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
