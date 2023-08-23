9d857e4a9b283162735b7a0c222c06a994dd28d7
endlessmomo <soohyuk96@naver.com>
2023-08-23 11:24:14 +0900

    [#10][feat] : JwtTokenProvider Claims 추출 및 검증 메소드 분리

--- src/main/java/f4/auth/global/security/jwt/JwtTokenProvider.java
+++ src/main/java/f4/auth/global/security/jwt/JwtTokenProvider.java
@@ -2,13 +2,16 @@ package f4.auth.global.security.jwt;
 
 import f4.auth.domain.user.dto.request.CreateTokenDto;
 import f4.auth.global.constant.CustomErrorCode;
+import f4.auth.global.exception.CustomException;
 import f4.auth.global.redis.RedisService;
 import f4.auth.global.security.jwt.exception.ExpiredTokenException;
 import f4.auth.global.security.jwt.exception.InvalidTokenException;
+import f4.auth.global.security.jwt.exception.UnSupportedTokenException;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -26,7 +29,7 @@ public class JwtTokenProvider {
   private final RedisService redisService;
 
   @Value("${jwt.secret}")
-  private String SECRET_KEY;
+  private String key;
 
   @Value("${jwt.token.access-expiration-time}")
   private Long atkDuration;
@@ -46,44 +49,62 @@ public class JwtTokenProvider {
   }
 
   public String createAccessToken(CreateTokenDto createTokenDto) {
-    return createToken(createTokenDto, atkDuration);
+    return createToken(createTokenDto, atkDuration, "access-token");
   }
 
   public String createRefreshToken(CreateTokenDto createTokenDto) {
-    return createToken(createTokenDto, rtkDuration);
+    return createToken(createTokenDto, rtkDuration, "refresh-token");
   }
 
-  private String createToken(CreateTokenDto createTokenDto, long time) {
+  private String createToken(CreateTokenDto createTokenDto, long time, String subject) {
     Claims claims = Jwts.claims();
     claims.put("userId", createTokenDto.getId());
     claims.put("username", createTokenDto.getUsername());
     claims.put("email", createTokenDto.getEmail());
+    claims.put("role", createTokenDto.getRole());
 
     Date now = new Date();
 
     return Jwts.builder()
         .setHeaderParam("typ", "JWT")
+        .setSubject(subject)
         .setClaims(claims)
         .setIssuedAt(now)
         .setExpiration(new Date(now.getTime() + time))
-        .signWith(getSigningKey(SECRET_KEY), SignatureAlgorithm.HS256)
+        .signWith(getSigningKey(key), SignatureAlgorithm.HS256)
         .compact();
   }
 
-  public Claims extractAllClaims(String token) {
-    //        if (redisService.hasBlackList(token)) {
-    //            throw AlreadyLogoutException.EXCEPTION;
-    //        }
+  public boolean validateToken(String token) {
+    if (redisService.hasBlackList(token)) {
+      throw new CustomException(CustomErrorCode.ALREADY_USER_LOGOUT);
+    }
     try {
-      return Jwts.parserBuilder()
-          .setSigningKey(getSigningKey(SECRET_KEY))
+      Jwts.parserBuilder()
+          .setSigningKey(getSigningKey(key))
           .build()
-          .parseClaimsJws(token)
-          .getBody();
+          .parseClaimsJws(token);
     } catch (ExpiredJwtException e) {
       throw new ExpiredTokenException(CustomErrorCode.EXPIRED_ACCESS_TOKEN);
     } catch (InvalidTokenException e) {
       throw new InvalidTokenException(CustomErrorCode.INVALID_ACCESS_TOKEN);
+    }
+    return false;
+  }
+
+  public Claims extractAllClaims(String token) {
+    if(!validateToken(token)){
+      throw new CustomException(CustomErrorCode.EMPTY_ACCESS_TOKEN);
+    }
+
+    try {
+      return Jwts.parserBuilder()
+          .setSigningKey(getSigningKey(key))
+          .build()
+          .parseClaimsJws(token)
+          .getBody();
+    } catch (ExpiredJwtException e) {
+      throw new UnSupportedTokenException(CustomErrorCode.UNSUPPORTED_TOKEN);
     }
   }
 
@@ -101,7 +122,7 @@ public class JwtTokenProvider {
 
   public Date getExpiredTime(String token) {
     return Jwts.parserBuilder()
-        .setSigningKey(getSigningKey(SECRET_KEY))
+        .setSigningKey(getSigningKey(key))
         .build()
         .parseClaimsJws(token)
         .getBody()
