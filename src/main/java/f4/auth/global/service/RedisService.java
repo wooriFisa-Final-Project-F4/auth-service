edd72085967d33cf3471d1345e14c718cd513cd6
endlessmomo <soohyuk96@naver.com>
2023-08-30 18:45:33 +0900

    [#16][feat] Redis 의존성 주입 및 Config & Service 작성

---
+++ src/main/java/f4/auth/global/service/RedisService.java
@@ -0,0 +1,45 @@
+package f4.auth.global.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+@RequiredArgsConstructor
+public class RedisService {
+
+  private final StringRedisTemplate stringRedisTemplate;
+  private final RedisTemplate<String, String> blackListTemplate;
+
+  public String getData(String key) {
+    ValueOperations<String, String> operations = stringRedisTemplate.opsForValue();
+    return operations.get(key);
+  }
+
+  public void setData(String key, String value) {
+    ValueOperations<String, String> operations = stringRedisTemplate.opsForValue();
+    operations.set(key, value);
+  }
+
+  public void setDataExpire(String key, String value, Duration duration) {
+    ValueOperations<String, String> operations = stringRedisTemplate.opsForValue();
+    operations.set(key, value, duration);
+  }
+
+  public void deleteData(String key) {
+    stringRedisTemplate.delete(key);
+  }
+
+  public void setBlackList(String key, Duration duration) {
+    ValueOperations<String, String> operations = blackListTemplate.opsForValue();
+    operations.set(key, "logout", duration);
+  }
+
+  public boolean hasBlackList(String key) {
+    return Boolean.TRUE.equals(blackListTemplate.hasKey(key));
+  }
+}
