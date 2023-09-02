6d7e0c6b2d9ce4f05c38e9b5d919beec593cc70e
endlessmomo <soohyuk96@naver.com>
2023-09-03 01:20:08 +0900

    [#18][feat] Jwt값 추춣하는 메소드 작성 및 dto 수정

--- src/main/java/f4/auth/global/constant/CustomErrorCode.java
+++ src/main/java/f4/auth/global/constant/CustomErrorCode.java
@@ -9,11 +9,13 @@ public enum CustomErrorCode {
 
   // Bad Request 400
   ALREADY_REGISTERED_MEMBER("/user/v1/signup", 400, "이미 회원 가입한 계정이 존재합니다."),
+  ALREADY_LOGOUT_USER("/auth/v1/token/reissue" ,400, "이미 로그아웃된 유저입니다."),
   CAN_NOT_ENCRYPT("/user/v1/signup", 400, "비밀번호를 암호화 할 수 없습니다."),
   NOT_VALID_LOGIN_PASSWORD("/user/v1/login", 400, "비밀번호가 틀렸습니다."),
 
   // Unathorized 401
   INVALID_ACCESS_TOKEN("user/v1/login", 401, "잘못된 토큰 입니다"),
+  INVALID_REFRESH_TOKEN("user/v1/reIssue", 401, "잘못된 토큰 입니다"),
   UNSUPPORTED_TOKEN("user/v1/login", 401, "지원하지 않는 토큰입니다"),
   EXPIRED_ACCESS_TOKEN("user/v1/login", 401, "만료된 엑세스토큰 입니다"),
   EXPIRED_REFRESH_TOKEN("user/v1/login", 401, "만료된 리프레시토큰 입니다"),
--- src/main/java/f4/auth/global/security/jwt/JwtTokenProvider.java
+++ src/main/java/f4/auth/global/security/jwt/JwtTokenProvider.java
@@ -1,7 +1,12 @@
 package f4.auth.global.security.jwt;
 
-import f4.auth.domain.auth.dtto.CreateTokenDto;
+import f4.auth.domain.auth.dto.CreateTokenDto;
+import f4.auth.global.constant.CustomErrorCode;
+import f4.auth.global.exception.CustomException;
+import f4.auth.global.security.jwt.exception.ExpiredTokenException;
+import f4.auth.global.security.jwt.exception.InvalidTokenException;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -10,9 +15,11 @@ import java.security.Key;
 import java.util.Date;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class JwtTokenProvider {
@@ -25,9 +32,6 @@ public class JwtTokenProvider {
 
   @Value("${jwt.token.refresh-token-duration}")
   private Long rtkDuration;
-
-  @Value("${jwt.token.header}")
-  private String header;
 
   private Key getSigningKey(String secretKey) {
     byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
@@ -44,6 +48,7 @@ public class JwtTokenProvider {
     return Jwts.builder()
         .setHeaderParam("typ", "JWT")
         .setClaims(claims)
+        .setSubject(createTokenDto.getEmail())
         .setIssuedAt(now)
         .setExpiration(new Date(now.getTime() + atkDuration))
         .signWith(getSigningKey(SECRET_KEY), SignatureAlgorithm.HS512)
@@ -64,5 +69,21 @@ public class JwtTokenProvider {
         .setExpiration(new Date(now.getTime() + rtkDuration))
         .signWith(getSigningKey(SECRET_KEY), SignatureAlgorithm.HS512)
         .compact();
+  }
+
+  public Claims extractAllClaims(String token) {
+    try {
+      return Jwts.parserBuilder()
+          .setSigningKey(getSigningKey(SECRET_KEY))
+          .build()
+          .parseClaimsJws(token)
+          .getBody();
+    } catch (ExpiredJwtException e) {
+      throw new ExpiredTokenException(CustomErrorCode.EXPIRED_ACCESS_TOKEN);
+    } catch (InvalidTokenException e) {
+      throw new InvalidTokenException(CustomErrorCode.INVALID_ACCESS_TOKEN);
+    } catch(Exception e){
+      throw new CustomException(CustomErrorCode.INVALID_REFRESH_TOKEN);
+    }
   }
 }
