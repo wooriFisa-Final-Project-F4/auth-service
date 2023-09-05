0d7ffd995c9c3d5c1757d27558ebdfd2fe7837b1
endlessmomo <soohyuk96@naver.com>
2023-09-05 10:18:22 +0900

    [#23][feat] Mock Service와 통신하기 위한 Feign 등록 및 dto 맞추기

---
+++ src/main/java/f4/auth/domain/user/service/feign/WooriMockServiceApi.java
@@ -0,0 +1,15 @@
+package f4.auth.domain.user.service.feign;
+
+import f4.auth.domain.user.service.feign.dto.request.LinkingRequestDto;
+import f4.auth.domain.user.service.feign.dto.response.ApiResponse;
+import javax.validation.Valid;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(value = "Mock", url = "https://5ca4-14-32-118-124.ngrok-free.app")
+public interface WooriMockServiceApi {
+
+  @PostMapping("/woori/account/v1/linking")
+  ApiResponse<?> linkingAccount(@Valid @RequestBody LinkingRequestDto linkingRequestDto);
+}
